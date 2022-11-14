ee784774e2ed2ecda5b07ec149b45a2d47a64403
wonderful <390969468@qq.com>
2022-11-14 14:02:28 +0800

    my change

--- main.go
+++ main.go
@@ -6,12 +6,20 @@ import (
 )
 
 func main() {
-	var num1 *leetcode.MyNode = &leetcode.MyNode{Val: 2,
-		Next: &leetcode.MyNode{Val: 4,
+	var num1 *leetcode.MyNode = &leetcode.MyNode{
+		Val: 2,
+		Next: &leetcode.MyNode{
+			Val:  4,
 			Next: &leetcode.MyNode{Val: 3},
 		},
 	}
-	var num2 *leetcode.MyNode = &leetcode.MyNode{Val: 5, Next: &leetcode.MyNode{Val: 6, Next: &leetcode.MyNode{Val: 6}}}
+	var num2 *leetcode.MyNode = &leetcode.MyNode{
+		Val: 5,
+		Next: &leetcode.MyNode{
+			Val:  6,
+			Next: &leetcode.MyNode{Val: 5},
+		},
+	}
 	resNode := leetcode.AddTowNumberByNodeList(num1, num2)
 	for resNode != nil {
 		fmt.Println(*resNode)
